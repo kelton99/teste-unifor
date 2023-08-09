3ae419faa4a589a91d3e7a64e4fab356dee9b772
kelton99 <keltonfreitas53@gmail.com>
2023-08-09 13:26:18 -0300

    projeto criado

---
+++ src/main/java/com/kelton/testeunifor/TesteUniforApplication.java
@@ -0,0 +1,13 @@
+package com.kelton.testeunifor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUniforApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteUniforApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelton/testeunifor/TesteUniforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelton.testeunifor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteUniforApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
